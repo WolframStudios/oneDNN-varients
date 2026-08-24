fe87983145d98ec8f64e77afd4f9416ffb6a560c
Ovchinnikov Dmitriy <dmitriy.ovchinnikov@intel.com>
2026-08-24 04:37:40 -0700

    build: enable MSVC C4267 warnings for CPU

--- src/cpu/x64/cpu_isa_traits.hpp
+++ src/cpu/x64/cpu_isa_traits.hpp
@@ -55,16 +55,19 @@
 #define XBYAK_STRICT_CHECK_MEM_REG_SIZE 0
 #endif
 
+#include "common/compiler_workarounds.hpp"
+
 #if defined(_MSC_VER) && !defined(__INTEL_COMPILER)
-/* turn off `size_t to other-type implicit casting` warning
- * currently we have a lot of jit-generated instructions that
- * take uint32_t, but we pass size_t (e.g. due to using sizeof).
- * FIXME: replace size_t parameters with the appropriate ones */
+/* Xbyak is a third-party header, keep its `size_t to other-type implicit
+ * casting` warnings suppressed. */
+#pragma warning(push)
 #pragma warning(disable : 4267)
 #endif
-#include "common/compiler_workarounds.hpp"
 #include "xbyak/xbyak.h"
 #include "xbyak/xbyak_util.h"
+#if defined(_MSC_VER) && !defined(__INTEL_COMPILER)
+#pragma warning(pop)
+#endif
 
 namespace dnnl {
 namespace impl {
